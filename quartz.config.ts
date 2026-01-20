e791012faca0ffded3a8cbe6f536c1c12b8770c2
135530578+luisadow@users.noreply.github.com <135530578+luisadow@users.noreply.github.com>
2026-01-20 15:50:00 +0100

    fix: set baseUrl for GitHub Pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "de-DE",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "luisadow.github.io/theo-iii-wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
